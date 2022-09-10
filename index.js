757bdb3f5798554a1c9c45ccb010cd19e31cd0f4
Razhatawka <nataliia85sakhnova@gmail.com>
2022-09-10 15:19:32 +0300

    работает, но выдает ошибку. и со стилями не дружит

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Сделайте интерфейс для анонимных комментариев со спамфильтром. Необходимо реализовать отображение и добавление сообщений,
 // а также функцию `checkSpam(str)`, заменяющую `'viagra'` или `'XXX'` на `***` Функция должна быть нечувствительна к регистру:
-// let comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
-// let comment2 = checkSpam('free xxx'); //результат free ***
-// let comment3 = checkSpam("innocent rabbit"); //результат innocent rabbit
 
 checkSpam = (str) => {
-    // проверяем есть ли в заметке 'viagra'или 'XXX'
-    // если нету, то вызываем функцию sendComment
-    // если есть, то заменяем  'viagra' или 'XXX' на `***` и вызываем функцию sendComment
-    // функция sendComment: клик - создание div - подключение классов - внутрь добавляем содержание инпута;
+    const reg = /viagra|XXX/gi; //заменить на "***" i - замены без учета регистра, g - заменить все совпадения
+    const comment = input.value.replace(reg, "***"); //готовый и проверенный комментарий
+    const p = document.createElement("p");
+    p.appendChild(document.createTextNode(input.value));
+    cont.appendChild(p); //создание div - подключение классов - внутрь добавляем содержание инпута;
 }
 
-button_send.addEventListener(`click`, checkSpam(str)); //вызываем функцию по клику на кнопку Send
+button_send.addEventListener(`click`, checkSpam); //вызываем функцию по клику на кнопку Send
+
+// "2019 год подходит в конце концов"
+// /конц.*/
+// .+
+// .*
+// /конц[eао]+/
+// /конц(е|ов)/
+// /конце|концов/
