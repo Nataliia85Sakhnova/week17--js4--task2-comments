e58cdda9657e209f37b7931dd9cb712be485a129
Razhatawka <nataliia85sakhnova@gmail.com>
2022-09-10 15:26:13 +0300

    работает, но выдает ошибку. и со стилями не дружит

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Сделайте интерфейс для анонимных комментариев со спамфильтром. Необходимо реализовать отображение и добавление сообщений,
 // а также функцию `checkSpam(str)`, заменяющую `'viagra'` или `'XXX'` на `***` Функция должна быть нечувствительна к регистру:
 
-checkSpam = (str) => {
+checkSpam = () => {
     const reg = /viagra|XXX/gi; //заменить на "***" i - замены без учета регистра, g - заменить все совпадения
     const comment = input.value.replace(reg, "***"); //готовый и проверенный комментарий
     const p = document.createElement("p");
     p.appendChild(document.createTextNode(input.value));
     cont.appendChild(p); //создание div - подключение классов - внутрь добавляем содержание инпута;
+    p.classList.add("cont");
 }
 
 button_send.addEventListener(`click`, checkSpam); //вызываем функцию по клику на кнопку Send
