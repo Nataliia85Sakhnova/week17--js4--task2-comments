ee999df3eecfef2ce31a29b2f7c467281008f2c5
Razhatawka <nataliia85sakhnova@gmail.com>
2022-10-21 16:20:01 +0300

    пишу функцию удочерения

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 
 // Сделайте интерфейс для анонимных комментариев со спамфильтром. Необходимо реализовать отображение и добавление сообщений,
 // а также функцию `checkSpam(str)`, заменяющую `'viagra'` или `'XXX'` на `***` Функция должна быть нечувствительна к регистру:
-
 // Доработать сохранение комментариев из прошлого домашнего задания через веб-хранилище,
 // дать возможность пользователю сохранять свое имя и аватарку при вводе и запоминать их.
 // Аватарку задаем в виде ссылки на картинку в интернете. Работу с файлами тут использовать не нужно.*
 
 const new_comment = (name, url, comment) => {
-    const comments_conteiner = document.createElement("div");
-    comments_conteiner.classList.add("user");
+    const comments_container = document.createElement("div"); //создали контейнер для комментария
+    comments_container.classList.add("user"); //присвоили ему класс
 
     const img = document.createElement("img");
     img.classList.add("img");
@@ -17,92 +16,67 @@ const new_comment = (name, url, comment) => {
     img.src = url;
 
     const name_user = document.createElement("p");
-    name_user.textContent = "krakozyabra";
+    name_user.textContent = "name_user.textContent";
 
     const comment_user = document.createElement("p");
-    comment_user.textContent = ";dkfgjbsd.fkbjnsdfjbn";
+    comment_user.textContent = "comment_user.textContent";
 
-    console.log(comments_conteiner);
-    console.log(img);
-    console.log(name_user);
-    console.log(comment_user);
+    // console.log(comments_container);
+    // console.log(img);
+    // console.log(name_user);
+    // console.log(comment_user);
+
+    // напишем функцию, чтоб кнтейнер всех удочерял, и пропишем вызов
+    const adopt = (a) => {
+        const child = comments_container.appendChild(a);
+        console.log(child);
+    }
+    adopt(img);
+    adopt(name_user);
+    adopt(comment_user);
+
 }
+
+new_comment('Diana', "https://thispersondoesnotexist.com/image", "текст из вызова функции");
+
 // добиться, что бы в консоль попадали все элементы с нужными значениями.
 // два выводятся правильно, осталось поле с именем и поле с комментарием.
 // Задание на звездочку, удочерить все элементы родительскому comment_container
-// и что бы при выводе его в консоль выходило что-то подобное
-// <div class="непомню-какой"> 
-//   <img class="img" alt="avatar" src="http\\тот адрес какой введн при вызове фукнции">
-//   <p>Diana(имя должно быть таким, какое прописано при вызове функции)</p>
-//   <p>комментарий, который передан при вызове функции</p>
-// </div>
-// последовательность расположения атрибутов внутри тегов может быть разная. Это не важно, главное, что бы они там были.
 
-new_comment('Diana', "https://thispersondoesnotexist.com/image", "lskjbndfk;vslgbm;sfgbkn");
+checkSpam = () => {
+    const name = document.getElementById('name').value
+    console.log(name)
+    const checkSpam = () => {
 
+        const name = document.getElementById('name').value;
+        const cont = document.getElementById('cont')
 
-// <!-- <div class="user">
-// <img src="https://thispersondoesnotexist.com/image" alt="avatar" class="img">
-// <p>Name</p>
-// <p>Comment</p>
-// </div> -->
+        const ava = document.getElementById('ava').value
+        console.log(ava);
+        const img = document.createElement('div').classList.add("img");
 
-const checkSpam = () => {
-
-    //создаем переменную имя:
-    const name = document.getElementById('name').value;
-    const cont = document.getElementById('cont')
-
-    //создаем переменную аватарка:
-    // const ava = document.getElementById('ava').value;
-    // ava.classList.add("ava");
-    // ava.src = document.getElementById('ava').value;
-    // ava.alt = "avatar";
-    // console.log(ava.alt);
-
-    // 1 взять значение инпута (ссылка на картинку) https://thispersondoesnotexist.com/
-    // 2 создать элемент картинка (img)
-    // 3 сделать свойство картинки .src ссылкой на картинку (из первого пункта)
-    // 4 сделать свойство .alt равное чему-то (у тебя это строка avatar)
-    // 5 добавить элемент (пункт2) куда-то (так же как ты добавляешь ниже текст)
-
-
-    // создаем комментарий:
-    const reg = /viagra|XXX/gi; //заменить на "***" i - замены без учета регистра, g - заменить все совпадения
-    const comment = document.getElementById('comment').value.replace(reg, "***"); //готовый и проверенный комментарий
-    const p = document.createElement("p");
-    p.appendChild(document.createTextNode(comment));
-    p.classList.add("p");
-    cont.appendChild(p); //создание div - подключение классов - внутрь добавляем содержание инпута;
+        const reg = /viagra|XXX/gi; //заменить на "***" i - замены без учета регистра, g - заменить все совпадения
+        const comment = document.getElementById('comment').value.replace(reg, "***"); //готовый и проверенный комментарий
+        const p = document.createElement("p");
+        button_send.addEventListener(`click`, checkSpam);
+    }
 }
 
-button_send.addEventListener(`click`, checkSpam); //вызываем функцию по клику на кнопку Send
-
-// "2019 год подходит в конце концов"
-// /конц.*/
-// .+
-// .*
-// /конц[eао]+/
-// /конц(е|ов)/
-// /конце|концов/
 
 
 
-// код Дианы:
-// const button = document.querySelector('.button-div');
-// const container = document.querySelector('.container');
 
-// button.addEventListener('click', () => {
-//   const name = document.querySelector('.name').value;
+//создаем переменную аватарка:
+// const ava = document.getElementById('ava').value;
+// ava.classList.add("ava");
+// ava.src = document.getElementById('ava').value;
+// ava.alt = "avatar";
+// console.log(ava.alt);
 
-//   const divImg = document.createElement('div');
-//   divImg.classList.add('img-div');
-//   // const ull = 'url(https://thispersondoesnotexist.com/image)';
-//   console.log(`url(${name})`);
-//   divImg.style.backgroundImage = `url(${name})`;
-//   container.appendChild(divImg);
-
-
-//   // document.querySelector('.img-div');
-//   // console.dir(divImg);
-// })
+// console.log(document.querySelector(".img").style.backgroundImage)
+// block.style.backgroundImage = "url
+// 1 взять значение инпута (ссылка на картинку) https://thispersondoesnotexist.com/
+// 2 создать элемент картинка (img)
+// 3 сделать свойство картинки .src ссылкой на картинку (из первого пункта)
+// 4 сделать свойство .alt равное чему-то (у тебя это строка avatar)
+// 5 добавить элемент (пункт2) куда-то (так же как ты добавляешь ниже текст)
