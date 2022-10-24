94c0c2dd66e906ec4377d972f65441dfeb807035
Razhatawka <nataliia85sakhnova@gmail.com>
2022-10-24 16:09:12 +0300

    опять все переписываю, пока нифига не работает

--- index.js
+++ index.js
@@ -1,59 +1,38 @@
-// "use strict" - высшее общество
-
-// Сделайте интерфейс для анонимных комментариев со спамфильтром. Необходимо реализовать отображение и добавление сообщений,
-// а также функцию `checkSpam(str)`, заменяющую `'viagra'` или `'XXX'` на `***` Функция должна быть нечувствительна к регистру:
-// Доработать сохранение комментариев из прошлого домашнего задания через веб-хранилище,
-// дать возможность пользователю сохранять свое имя и аватарку при вводе и запоминать их.
-// Аватарку задаем в виде ссылки на картинку в интернете. Работу с файлами тут использовать не нужно.*
-
-
-
-
-
-
-
-
-
-
-
-
+// пишу функцию, где создаю контейнер, имя, аву и коммент
+// присваиваю им всем стили и чо там еще надо
+// присваиваю им всем значения из полей ввода
+// пишу функцию, где контейнер удочеряет имя, аву и коммент
 
 const new_comment = (name, url, comment) => {
-    const comments_container = document.createElement("div"); //создали контейнер для комментария
-    comments_container.classList.add("user"); //присвоили ему класс
+    const comments_container = document.createElement("div");
+    comments_container.classList.add("user");
 
-    const img = document.createElement("img");
-    img.classList.add("img");
-    img.alt = "avatar";
-    img.src = url;
+    name = document.createElement("p");
+    name.textContent = document.getElementById("name").value;
 
-    const name_user = document.createElement("p");
-    // name_user.textContent = "name_user.textContent";
+    url = document.createElement("img");
+    url.textContent = "попку в топку";
+    // // как присвоить ему ссыль из поля ввода?   document.getElementById("ava").value;
+    // url.classList.add("img");
+    // url.alt = "avatar";
 
-    const comment_user = document.createElement("p");
-    // comment_user.textContent = "comment_user.textContent";
+    comment = document.createElement("p");
+    comment.textContent = "шавка на лавке";
+    // document.getElementById("comment").value;
 
     const adopt = (a) => {
         const child = comments_container.appendChild(a);
         console.log(child);
     }
-    adopt(img);
-    adopt(name_user);
-    adopt(comment_user);
 
-    console.dir(comments_container);
+    adopt(name);
+    adopt(url);
+    adopt(comment);
 
+    // console.dir(comments_container);
 }
 
-new_comment('Diana', "https://thispersondoesnotexist.com/image", "наконец-то правильно выводится");
-
-
-
-
-
-
-
-
+new_comment();
 
 
 // добиться, что бы в консоль попадали все элементы с нужными значениями.
@@ -97,3 +76,11 @@ checkSpam = () => {
 // 3 сделать свойство картинки .src ссылкой на картинку (из первого пункта)
 // 4 сделать свойство .alt равное чему-то (у тебя это строка avatar)
 // 5 добавить элемент (пункт2) куда-то (так же как ты добавляешь ниже текст)
+
+// "use strict" - высшее общество
+
+// Сделайте интерфейс для анонимных комментариев со спамфильтром. Необходимо реализовать отображение и добавление сообщений,
+// а также функцию `checkSpam(str)`, заменяющую `'viagra'` или `'XXX'` на `***` Функция должна быть нечувствительна к регистру:
+// Доработать сохранение комментариев из прошлого домашнего задания через веб-хранилище,
+// дать возможность пользователю сохранять свое имя и аватарку при вводе и запоминать их.
+// Аватарку задаем в виде ссылки на картинку в интернете. Работу с файлами тут использовать не нужно.*
